404aaf22f04bb4f706bb60bb78a745b15ac25bf0
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-12-01 11:18:42 +0200

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <header className={styles.container}>
+      <div className={styles.flex}>
+        <p className={styles.logo}>
+          <strong>serve </strong>pro
+        </p>
+        <menu className={styles.menu}>
+          <li>Funkcijos</li>
+          <li>Ataskaitos</li>
+          <li>Sandėlys</li>
+          <li>Nustatymai</li>
+          <li>Atsijungti</li>
+        </menu>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
