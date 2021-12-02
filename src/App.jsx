49e0d61f842d2236f2521182557359a7d4dfacc1
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-12-02 13:05:53 +0200

    aside + functionsmenu and flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Aside from './components/Aside/Aside';
+import FunctionsMenu from './components/FunctionsMenu/FunctionsMenu';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="main-flex">
+        <Aside />
+        <main>
+          <FunctionsMenu />
+          {/* <ClientSearch /> */}
+        </main>
+      </div>
       <Footer />
     </div>
   );
