7ff4aa74a3f94c11bc4f86c891aecd4060b069e8
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-12-03 12:03:03 +0200

    searchdata component

--- src/components/ClientSearch/ClientSearch.jsx
+++ src/components/ClientSearch/ClientSearch.jsx
@@ -1,11 +1,13 @@
 import styles from './ClientSearch.module.css';
 import SearchTable from './SearchTable/SearchTable';
+import SearchData from './SearchData/SearchData';
 
 function ClientSearch() {
   return (
     <div className={styles.window}>
       <h2>Kliento paieška</h2>
       <SearchTable />
+      <SearchData />
     </div>
   );
 }
