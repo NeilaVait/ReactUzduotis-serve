3ae71b019542d2413ad44fa88111c829daefbf6f
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-12-05 18:32:55 +0200

    scrollbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { Scrollbars } from 'react-custom-scrollbars';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FunctionsPage from './pages/FunctionsPage';
 import HomePage from './pages/HomePage';
@@ -7,14 +8,18 @@ import ClientContextProvider from './components/contexts/ClientContext';
 function App() {
   return (
     <div className="App">
-      <ClientContextProvider>
-        <Router>
-          <Routes>
-            <Route path="/funkcijos" element={<FunctionsPage />} />
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-        </Router>
-      </ClientContextProvider>
+      <Scrollbars style={{ height: 700 }}>
+        <div className="scrollhost">
+          <ClientContextProvider>
+            <Router>
+              <Routes>
+                <Route path="/funkcijos" element={<FunctionsPage />} />
+                <Route path="/" element={<HomePage />} />
+              </Routes>
+            </Router>
+          </ClientContextProvider>
+        </div>
+      </Scrollbars>
     </div>
   );
 }
